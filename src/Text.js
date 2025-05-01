b73f19a338673dfef07b104d4152d5d41ef23b07
Chin Hong Ming <96917788+chm252@users.noreply.github.com>
2025-05-01 12:09:51 +0800

    Update Text.js

--- src/Text.js
+++ src/Text.js
@@ -5,11 +5,11 @@ function Text() {
     return (
         <div style = {{justifyContent: 'center', display :'flex'}}>
         <img src = {h} style={{ width: '10%', height: '10%', padding : '3%' }}></img>
-        <p style = {{textAlign : 'left',fontSize : '200%'}}>I am a Computer Science Undergraduate at NUS and <br/>
-        quite in love with Computer Security and AI. <br/>
+        <p style = {{textAlign : 'left',fontSize : '200%'}}>A software engineer who has <br/>
+        graduated from NUS Computer Science. <br/>
         <br/>
         Besides computers, I love playing basketball, badminton, cycling and doing art. 
-        <br/> 🐳 okay cool, have fun visiting
+        <br/> 🐳 
             </p>
         </div>
     )
